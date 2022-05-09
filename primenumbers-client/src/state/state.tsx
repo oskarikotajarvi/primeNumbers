0ebedb8b7ae83cc19e66d0a55c57cca9ad92d98a
Oskari Kotajärvi <okotajarvi@topcon.com>
2022-05-09 22:53:27 +0300

    Removed unnecessary React import

--- primenumbers-client/src/state/state.tsx
+++ primenumbers-client/src/state/state.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useReducer } from 'react';
+import { createContext, Dispatch, FC, ReactElement, Reducer, useContext, useReducer } from 'react';
 import { Action } from './reducer';
 
 export type State = {
@@ -9,14 +9,14 @@ const initialState: State = {
     useDarkTheme: true,
 };
 
-export const StateContext = createContext<[State, React.Dispatch<Action>]>([initialState, () => initialState]);
+export const StateContext = createContext<[State, Dispatch<Action>]>([initialState, () => initialState]);
 
 type StateProviderProps = {
-    reducer: React.Reducer<State, Action>;
-    children: React.ReactElement;
+    reducer: Reducer<State, Action>;
+    children: ReactElement;
 };
 
-export const StateProvider: React.FC<StateProviderProps> = ({ reducer, children }: StateProviderProps) => {
+export const StateProvider: FC<StateProviderProps> = ({ reducer, children }: StateProviderProps) => {
     const [state, dispatch] = useReducer(reducer, initialState);
     return <StateContext.Provider value={[state, dispatch]}>{children}</StateContext.Provider>;
 };
